d0f8565a78faf76088ba30158fdca5d9d71ffa5d
tsorinan <tsorinan@TSORINAN-T490.ro.oracle.com>
2021-10-28 14:39:17 +0300

    created adoption-list component

---
+++ src/components/adoption-list/adoption-list.jsx
@@ -0,0 +1,13 @@
+import '/adoption-list.style.scss';
+
+const AdoptionList = () =>{
+
+    return(
+        <div className="list">
+            
+        </div>
+    )
+
+}
+
+export default AdoptionList;
